20de54131552df7f321179452641f0d785bbd051
Ernst <TME17@pitt.edu>
2017-10-29 13:27:43 -0400

    Adding gradle file

---
+++ abracadbra/src/edu/pitt/lock/SoleniodLock.java
@@ -0,0 +1,19 @@
+package edu.pitt.lock;
+
+/**
+ * An implementation of the lock interface, which physically manages the soleniod, using GPIO pins.
+ */ 
+ public class SoleniodLock implements Lock
+ {
+	 @Override
+	 public void lock()
+	 {
+		 // Here we will lock
+	 }
+	 
+	 @Override
+	 public void unlock()
+	 {
+		 // Here we will unlock
+	 }
+ }
